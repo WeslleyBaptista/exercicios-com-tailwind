6d7464d1f76ab702f5591f30d4de330205e975f7
Weslley Baptista <117368101+WeslleyBaptista@users.noreply.github.com>
2025-02-20 10:00:54 -0300

    Initial commit

---
+++ Exercício Hero Card/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,ts,jsx,tsx}", // Define onde o Tailwind deve procurar classes CSS
+    ],
+    theme: {
+      extend: {},
+    plugins: [],
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicios-com-tailwind
+ Exercícios utilizando TailWindCSS que foram propostos pelo curso da B7Web.
